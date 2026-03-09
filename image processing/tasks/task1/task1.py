a2cdb241dbc06f3b8ff99ccca4833b74eafb5ccd
minamontaser <147017533+minamontaser@users.noreply.github.com>
2026-03-09 23:42:32 +0200

    Add files via upload

--- image processing/tasks/task1/task1.py
+++ image processing/tasks/task1/task1.py
@@ -2,7 +2,12 @@ import cv2 as cv
 import numpy as np
 
 #importing image
-task1_img = cv.imread(r"E:\Fury center\Projects\Machine_Learning\assigment_1_image.png")
+task1_img = cv.imread("assigment_1_image.png")
+owl_img= cv.imread("rog_owl.jpg")
+
+if task1_img is None and placeholder_img is None:
+    print("No images exists!")
+    exit()
 
 def rescaleFrame(frame: np.ndarray, scale: float = 0.5) -> np.ndarray:
     width = int(frame.shape[1] * scale)
@@ -25,8 +30,15 @@ def insert_text(frame: np.ndarray, text: str) -> np.ndarray:
                2, (255,0,255), 2)
     return frame
 
-#rescaling image
+def placehold(frame: np.ndarray, placeholder: np.ndarray, x: int = 240, y: int = 105) -> np.ndarray:
+    h, w = placeholder.shape[:2]
+
+    frame[y:y+h, x:x+w] = placeholder
+    return frame
+
+#rescaling images
 task1_img = rescaleFrame(task1_img)
+owl_img = rescaleFrame(owl_img, 0.075)
 
 #colored image
 print(f"image details: {task1_img.shape}")
@@ -46,8 +58,12 @@ id_text = input("Enter your id: ")
 id_image = insert_text(cropped_img, id_text)
 cv.imshow('id_embedded', id_image)
 
+#placeholder image
+placeholder_img = placehold(id_image, owl_img)
+cv.imshow('placeholder_img', placeholder_img)
+
 #export the final edit
-cv.imwrite("Mina_Montaser.jpg", id_image)
+cv.imwrite("Mina_Montaser.jpg", placeholder_img)
 
 cv.waitKey(0)
 cv.destroyAllWindows()
